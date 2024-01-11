66b997ebe08a62b46d9be2c03b04bb92a556cf5b
Michael R. Wolf <MichaelRWolf@att.net>
2024-01-11 17:52:08 -0500

    - t Split cyber-dojo file based on GPT recommendations.  It worked!

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -1,48 +1,31 @@
 # Forked from
 # https://cyber-dojo.org/kata/edit/xJxHW1
 
-def is_divisible_by(number, divisor):
-    return number % divisor == 0
+class FizzBuzz:
 
+    def __init__(self):
+        self.specs = [self.get_word(3)("Fizz"), self.get_word(5)("Buzz")]
 
-# def is_divisible_by_5(number):
-#     return is_divisible_by(number, 5)
-#
-#
-# def is_divisible_by_3(number):
-#     return is_divisible_by(number, 3)
-#
-#
-# def is_divisible_by_3_and_5(number):
-#     return is_divisible_by_3(number) and is_divisible_by_5(number)
+    def is_divisible_by(self, number, divisor):
+        return number % divisor == 0
 
+    def get_word(self, divisor):
+        def code_fn(code):
+            def result(number):
+                if self.is_divisible_by(number, divisor):
+                    return code
+                else:
+                    return ""
 
-def get_word(divisor):
-    def code_fn(code):
-        def result(number):
-            if is_divisible_by(number, divisor):
-                return code
-            else:
-                return ""
+            return result
 
+        return code_fn
+
+    def fizz_and_buzz(self, number):
+        result = ""
+        for spec in self.specs:
+            result += spec(number)
         return result
 
-    return code_fn
-
-
-fizz = get_word(3)("Fizz")
-buzz = get_word(5)("Buzz")
-specs = [fizz, buzz]
-
-
-def fizz_and_buzz(number):
-    result = ""
-    for spec in specs:
-        result += spec(number)
-    return result
-
-
-def fizzbuzz(number):
-    return fizz_and_buzz(number) or str(number)
-
-class FizzBuzz():
+    def fizzbuzz(self, number):
+        return self.fizz_and_buzz(number) or str(number)
--- tests/test_FizzBuzz.py
+++ tests/test_FizzBuzz.py
@@ -1,5 +1,26 @@
-from unittest import TestCase
+import unittest
+from src.FizzBuzz import FizzBuzz
 
+class FizzBuzzTest(unittest.TestCase):
 
-class TestFizzBuzz(TestCase):
-    pass
+    def setUp(self):
+        self.fizzbuzz = FizzBuzz()
+
+    def test_fizz(self):
+        self.assertEqual(self.fizzbuzz.fizzbuzz(3), "Fizz")
+        self.assertEqual(self.fizzbuzz.fizzbuzz(6), "Fizz")
+
+    def test_buzz(self):
+        self.assertEqual(self.fizzbuzz.fizzbuzz(5), "Buzz")
+        self.assertEqual(self.fizzbuzz.fizzbuzz(10), "Buzz")
+
+    def test_fizzbuzz(self):
+        self.assertEqual(self.fizzbuzz.fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(self.fizzbuzz.fizzbuzz(30), "FizzBuzz")
+
+    def test_numbers(self):
+        self.assertEqual(self.fizzbuzz.fizzbuzz(2), "2")
+        self.assertEqual(self.fizzbuzz.fizzbuzz(4), "4")
+
+if __name__ == '__main__':
+    unittest.main()
