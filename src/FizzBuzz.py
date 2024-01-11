97be760c567ce5c4f1184db890aeba1cf056349d
Michael R. Wolf <MichaelRWolf@att.net>
2024-01-11 18:16:14 -0500

    . a Rename based on ChatGPT suggestions.

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -4,22 +4,22 @@
 class FizzBuzz:
 
     def __init__(self):
-        self.specs = [self.get_word(3)("Fizz"), self.get_word(5)("Buzz")]
+        self.specs = [self.create_divisibility_checker(3)("Fizz"), self.create_divisibility_checker(5)("Buzz")]
 
-    def is_divisible_by(self, number, divisor):
-        return number % divisor == 0
-
-    def get_word(self, divisor):
-        def code_fn(code):
-            def result(number):
-                if self.is_divisible_by(number, divisor):
-                    return code
+    def create_divisibility_checker(self, check_value):
+        def divisibilityResponse(outputString):
+            def check_and_return_outputString(number):
+                if self.is_divisible_by(number, check_value):
+                    return outputString
                 else:
                     return ""
 
-            return result
+            return check_and_return_outputString
 
-        return code_fn
+        return divisibilityResponse
+
+    def is_divisible_by(self, number, divisor):
+        return number % divisor == 0
 
     def fizz_and_buzz(self, number):
         result = ""
